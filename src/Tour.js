e94211d02c99f9d79b0126defc6a8e5bbde37ed9
Blake Hansen <b.hansen9264@gmail.com>
2022-10-24 08:51:42 -0700

    Added Delete Buttons

--- src/Tour.js
+++ src/Tour.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const Tour = ({id, image, info, price, name}) => {
+const Tour = ({id, image, info, price, name, removeTour}) => {
   const [readMore, setReadMore] = useState(false);
   return (
     <article className="single-tour">
@@ -15,7 +15,7 @@ const Tour = ({id, image, info, price, name}) => {
         <button onClick={(()=> setReadMore(!readMore))}>
           {readMore?'show less' : 'read more '}</button>
         </p>
-        <button className='delete-btn'>DELETE</button>
+        <button className='delete-btn' onClick={()=> removeTour(id)}>DELETE</button>
       </footer>
     </article>
   )
